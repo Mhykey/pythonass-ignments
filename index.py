c1b6eb3b5ff6864493813057daf790cdfd9a8de6
Michael Joy <verifiedmichaelogunleke@gmail.com>
2025-08-29 12:17:39 +0100

    Week 5 Submission.

--- index.py
+++ index.py
@@ -1,23 +1,62 @@
-def modify_file():
-    try:
-        filename = input("Enter the filename to read: ")
+#Assignment 1
 
-        with open(filename, "r") as file:
-            content = file.read()
-
-        modified_content = content.upper()
-
-        with open("output.txt", "w") as file:
-            file.write(modified_content)
-
-        print("Modified file saved as output.txt")
-
-    except FileNotFoundError:
-        print("Error: File does not exist.")
-    except PermissionError:
-        print("Error: No permission to read this file.")
-    except Exception as e:
-        print("Unexpected error:", e)
+class FootballPlayer:
+    def __init__(self, name, position, club):
+        self.name = name
+        self.position = position
+        self.club = club
+    
+    def introduce(self):
+        return f"My name is {self.name}, I play as a {self.position} for {self.club}."
+    
+    def train(self):
+        return f"{self.name} is training hard on the pitch!"
 
 
-modify_file()
+class Goalkeeper(FootballPlayer):
+    def __init__(self, name, club, clean_sheets):
+
+      
+        super().__init__(name, "Goalkeeper", club)
+        self.clean_sheets = clean_sheets
+    
+    def _secret_training(self):
+        return f"{self.name} is secretly practicing penalty saves!"
+    
+ 
+    def train(self):
+        return f"{self.name} is practicing diving and shot-stopping!"
+
+
+player1 = FootballPlayer("Kevin De Bruyne", "Midfielder", "Manchester City")
+player2 = Goalkeeper("Alisson Becker", "Liverpool", 15)
+
+print(player1.introduce())
+print(player1.train())
+print(player2.introduce())
+print(player2.train())
+
+
+#Assignment 2
+
+
+class Vehicle:
+    def move(self):
+        return "Moving in some way..."
+
+class Car(Vehicle):
+    def move(self):
+        return "Driving on the road 🚗"
+
+class Bike(Vehicle):
+    def move(self):
+        return "Riding on two wheels 🚴"
+
+class Plane(Vehicle):
+    def move(self):
+        return "Flying in the sky ✈️"
+
+vehicles = [Car(), Bike(), Plane()]
+
+for v in vehicles:
+    print(v.move())
